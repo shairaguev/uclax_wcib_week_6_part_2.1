22f88efb76d638e6766b5d3c872cdb7fa80e4560
Shaira <shairaguev@gmail.com>
2019-11-28 16:18:17 -0800

    JavaScript Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
+window.onload = function (){
 console.log('Hello from js/scripts.js!');
+ //Congratulations Message
+
+var claim = document.getElementById('claim');
+var firstName = document.getElementById('inputFirstName');
 
 
-   randomize.addEventListener('click', result);
+claim.addEventListener('click', customName);
+
+function customName() {  if(firstName.value !== '') {
+    var name = firstName.value;
+    var congrats = 'Congratulations  ' + name + '! Please enjoy your treat!';
+    var congratsNote = document.querySelector('.congratsNote');
+    congratsNote.textContent = congrats;
+
+  }
+}
+
+//coffee or tea
+
+randomize.addEventListener (click,result);
 
   if(document.getElementById("coffee").checked) {
-   document.getElementById('imagesource').src='images/coffee.jpg';
+   document.getElementById('imagesource').src='images/freecoffee.jpg';
  } else if (document.getElementById("tea").checked) {
-   document.getElementById('imagesource').src='images/tea.jpg';
+   document.getElementById('imagesource').src='images/freetea.jpg';
  }
-
-
 
 
  story.textContent = newStory;
